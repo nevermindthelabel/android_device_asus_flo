64db60538993f650c4d60a8b37b6a3b91d612dfa
gtsiamp <gtsiamp@gmail.com>
2017-01-20 09:35:32 +0200

    Add 4.8 Kernel TC

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel details
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
+
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += vmalloc=340M
@@ -36,6 +37,7 @@ TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := krexus_flo_defconfig
 TARGET_VARIANT_CONFIG := krexus_flo_defconfig
 TARGET_SELINUX_CONFIG := krexus_flo_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 
 # Audio
 BOARD_USES_ALSA_AUDIO:= true
